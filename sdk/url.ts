42809ca4225d68d6ce15c7e86719e039024f9c8b
Luigi <luigidias@protonmail.com>
2024-03-04 14:06:55 -0300

    aaa

--- sdk/url.ts
+++
@@ -1,5 +0,0 @@
-export const relative = (link?: string | undefined) => {
-  const linkUrl = link ? new URL(link) : undefined;
-  const linkPath = linkUrl ? `${linkUrl.pathname}${linkUrl.search}` : undefined;
-  return linkPath;
-};
