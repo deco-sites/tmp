a145657a03b5cdaef7c491ef0e0514082559985e
Luigi <luigidias@protonmail.com>
2024-03-04 14:13:06 -0300

    aaaaaaaaaaaaaaa

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -2,13 +2,11 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $$$$$$$$$$$0 from "./apps/decohub.ts";
-import * as $$$$$$$$$$$1 from "./apps/site.ts";
+import * as $$$$$$$$$$$0 from "./apps/site.ts";
 
 const manifest = {
   "apps": {
-    "deco-sites/tmp/apps/decohub.ts": $$$$$$$$$$$0,
-    "deco-sites/tmp/apps/site.ts": $$$$$$$$$$$1,
+    "deco-sites/tmp/apps/site.ts": $$$$$$$$$$$0,
   },
   "name": "deco-sites/tmp",
   "baseUrl": import.meta.url,
